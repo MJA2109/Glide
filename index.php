c950a726face4195ea11a0c4cc901ac5c17452ec
MJA2109 <michaelandersonn@hotmail.com>
2014-07-21 00:30:10 +0100

    Set-up require.js. Added various libraries such as Boostrap, Less etc

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php require("templates/head.php"); ?>
+        <!-- Add your site or application content here -->
+        <p>Hello world! This is HTML5 Boilerplate.</p>
+        <div id = 'test'>
+            <p>this is crazy</p>
+        </div>
+    
+<?php require("templates/footer.php"); ?>
--- js/main.js
+++ js/main.js
@@ -1,1 +1,33 @@
+requirejs.config({
+    appDir: ".",
+    baseUrl: "js",
+    paths: { 
+        /* Load jquery from google cdn. On fail, load local file. */
+        'jquery': ['//ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min', 'vendor/jquery-1.10.2.min'],
+        'modernizr' : ['vendor/modernizr-2.6.2.min'],
+        'boilerPlugins' : ['plugins'],
+        'analytics' : ['analytics'],
+        'bootstrap': ['//netdna.bootstrapcdn.com/bootstrap/3.0.2/js/bootstrap.min', 'vendor/boostrap.min'],
+        'cssLess' : ['//cdnjs.cloudflare.com/ajax/libs/less.js/1.7.3/less.min', 'vendor/less-1.7.3.min']
+    },
+    shim:{
+    	'bootstrap': ['jquery']
+    }
+});
+
+require(['jquery', 
+		 'modernizr', 
+		 'boilerPlugins', 
+		 'analytics',
+		 'bootstrap',
+		 'cssLess'], function($) {
+    	console.log("working"); 
+    	return {};
+});
+
+
+
+
+
+
 
