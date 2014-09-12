f487c1eb8e071dc59c5e939b4686c1da67b7a39d
MJA2109 <michaelandersonn@hotmail.com>
2014-09-13 00:22:00 +0100

    Added basic image and data upload functionality.

---
+++ api/glideMobileAPI.php
@@ -0,0 +1,6 @@
+<?php
+
+	$new_image_name = $_FILES["file"]["name"];
+	echo "You are at glide mobile";
+
+?>
--- phoneApp/platforms/android/assets/www/js/index.js
+++ phoneApp/platforms/android/assets/www/js/index.js
@@ -1,66 +1,134 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+//Initialize application. Set global variables.
 var app = {
-    // Application Constructor
+    server: "http://ma.pickacab.com/test/test.php",
+    imageURI : "",
     initialize: function() {
         this.bindEvents();
     },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
     bindEvents: function() {
         document.addEventListener('deviceready', this.onDeviceReady, false);
     },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-        app.receivedEvent('deviceready');
         initializeEvents();
     },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        alert(id);
-    }
 };
 
 
+/**
+ * Name: initializeEvents
+ * Purpose: Initialize each event before use.
+ */
 function initializeEvents(){
-    captureReceipt();
+    $("#btnCaptureReceipt").click(function(){
+        captureReceipt();
+    });
 
-}
-
-
-function captureReceipt(){
-    $("#captureReceipt").click(function(){
-        navigator.camera.getPicture(onSuccess, onFail, { quality: 50, 
-        destinationType: Camera.DestinationType.FILE_URI, correctOrientation: false });
-
-        function onSuccess(imageURI) {
-            var image = document.getElementById('receipt');
-            image.src = imageURI;
-        }
-
-        function onFail(message) {
-            alert('Failed because: ' + message);
-        }
+    $("#btnUpload").click(function(){
+        uploadExpenseForm("#uploadExpenseForm");
     });
 }
+
+
+/**
+ * Name: captureReceipt
+ * Purpose: Acesses phone's camera to allow user to take picture. Returned image is placed in DOM
+ */
+function captureReceipt(){
+    navigator.camera.getPicture(onSuccess, onFail, { quality: 50, 
+                    destinationType: Camera.DestinationType.FILE_URI, correctOrientation: false });
+    
+    function onSuccess(imageURI) {
+        $("<img id = 'receipt' src = ''>").appendTo("#uploadedImage");
+        app.imageURI = imageURI;
+        var image = document.getElementById('receipt');
+        image.src = imageURI;
+        alert("working");
+    }
+
+    function onFail(message) {
+        alert('Failed because: ' + message);
+    }
+}
+
+/**
+ * Name: uploadReceipt
+ * Purpose: Upload captured receipt to the Glide server.
+ */
+function uploadReceipt(){
+
+    $.mobile.loading("show", {
+        text: "Uploading data...",
+        textVisible: true,
+        theme: "z"
+    });
+
+    var imageURI = app.imageURI;
+    var options = new FileUploadOptions();
+    options.fileKey = "file";
+    options.fileName = imageURI.substr(imageURI.lastIndexOf('/')+1);
+    options.mimeType = "image/jpeg";
+
+    var params = {};
+    params.value1 = "test";
+    params.value2 =  "param";
+    options.params = params;
+
+    var fileTrans = new FileTransfer();
+    fileTrans.upload(imageURI, encodeURI(app.server), uploadComplete, uploadFailed, options);
+
+    alert(JSON.stringify(options));
+
+    function uploadFailed(error){
+        alert("upload failed " + error.target);
+    }
+
+    function uploadComplete(data){
+        alert("upload successful" + data.response);
+        resetUploadExpenseForm();
+        $.mobile.loading("hide");
+    }
+}
+
+
+/**
+ * Name: uploadExpenseForm
+ * Purpose: Get data from form and send to Glide server.
+ * @param form - string : form to be processed.
+ */
+function uploadExpenseForm(form){
+    var frm = $(form);
+    var data = frm.serialize();
+    $.ajax({
+        type: "post",
+        url: app.server,
+        data: data,
+        beforeSend: function() {
+            uploadReceipt(); 
+        },
+        complete: function(){
+        
+        }, 
+        success: function(data){
+
+        },
+        error: function(data){
+            alert("Ajax Error " + data);
+        }               
+    });   
+}
+
+/**
+ * Name: resetUploadExpenseForm
+ * Purpose: Reset image & data input fields to null
+ */
+function resetUploadExpenseForm(){
+    $("#uploadMerchant, #uploadDate, #uploadCost, #uploadComment").val("");
+    $("#receipt").remove();
+}
+
+
+
+
+
+
+
