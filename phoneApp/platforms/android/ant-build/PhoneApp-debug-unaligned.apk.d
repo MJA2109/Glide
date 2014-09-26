8416bb2cbd2901cd0aae099196c432f6d7f4855b
MJA2109 <michaelandersonn@hotmail.com>
2014-09-26 09:40:34 +0100

    Clean-up

--- phoneApp/platforms/android/ant-build/PhoneApp-debug-unaligned.apk.d
+++ phoneApp/platforms/android/ant-build/PhoneApp-debug-unaligned.apk.d
@@ -1,4 +1,4 @@
-/Applications/MAMP/htdocs/Glide/phoneApp/platforms/android/ant-build/PhoneApp-debug-unaligned.apk : \
-/Applications/MAMP/htdocs/Glide/phoneApp/platforms/android/ant-build/PhoneApp.ap_ \
-/Applications/MAMP/htdocs/Glide/phoneApp/platforms/android/ant-build/classes.dex \
+/Applications/Mamp/htdocs/Glide/phoneApp/platforms/android/ant-build/PhoneApp-debug-unaligned.apk : \
+/Applications/Mamp/htdocs/Glide/phoneApp/platforms/android/ant-build/PhoneApp.ap_ \
+/Applications/Mamp/htdocs/Glide/phoneApp/platforms/android/ant-build/classes.dex \
 /Applications/MAMP/htdocs/Glide/phoneApp/platforms/android/CordovaLib/ant-build/classes.jar \
