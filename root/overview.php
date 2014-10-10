78425f44552bfcf45eebc94e18811db8c0abe0aa
MJA2109 <michaelandersonn@hotmail.com>
2014-10-10 17:43:24 +0100

    Added basic layout for overview page.

--- root/overview.php
+++ root/overview.php
@@ -1,37 +1,46 @@
 <?php
 	sleep(3);
 ?>
-<div class = "widget">
-	<div><h3>New Users</h3></div>
-	<div>
-		New user info
+<div class = "sectionWrapper">
+	<div class = "section">
+		<div class = "widget panel panel-default">
+			<div class="panel-heading"><span class="glyphicon glyphicon-user">Active Users</span></div>
+		  	<div class="panel-body">
+		    	User content
+		 	</div>
+		</div>
+		<div class = "widget panel panel-default">
+			<div class="panel-heading"><span class="glyphicon glyphicon-arrow-up">New Expenses</span></div>
+		  	<div class="panel-body">
+		    	expense content
+		 	</div>
+		</div>
+		<div class = "widget panel panel-default">
+			<div class="panel-heading"><span class="glyphicon glyphicon-flag">Unprocessed Payments</span></div>
+		  	<div class="panel-body">
+		    	unprocessed
+		 	</div>
+		</div>
 	</div>
-</div>
-<div class = "widget">
-	<div><h3>New Expenses</h3></div>
-	<div>
-		New expense data
-	</div>
-</div>
-<div class = "widget">
-	<div><h3>Awaiting Processing</h3></div>
-	<div>
-		Awaiting processing info
-	</div>
-</div>
-<div class = "widget">
-	<div><h3>Liabilities</h3></div>
-	<div>
-		Monies owed
-	</div>
-</div>
-<div class = "widget">
-	<div><h3>Notes</h3></div>
-	<div></div>
-</div>
-<div class = "widget">
-	<div><h3>Auxiliary</h3></div>
-	<div>
-		more data
+
+	<div class = "section">
+		<div class = "widget panel panel-default">
+			<div class="panel-heading"><span class="glyphicon glyphicon-usd">Liabilities</span></div>
+		  	<div class="panel-body">
+		    	liability
+		 	</div>
+		</div>
+		<div class = "widget panel panel-default">
+			<div class="panel-heading"><span class="glyphicon glyphicon-paperclip">Notes</span></div>
+		  	<div class="panel-body">
+		    	notes
+		 	</div>
+		</div>
+		<div class = "widget panel panel-default">
+			<div class="panel-heading"><span class="glyphicon glyphicon-user">auxiliary</span></div>
+		  	<div class="panel-body">
+		    	auxiliary
+		 	</div>
+		</div>
 	</div>
 </div>
